78b2d6747b7058d2c0b2045f737ad17840f0c66f
Rocío Torres <rociotorresgarcia6@gmail.com>
2026-03-06 13:05:30 +0100

    Estoy empezando la parte de fotografía y he puesto un texto arriba y las imágenes de las cámaras

--- src/pages/photography/PhotographyPage.vue
+++ src/pages/photography/PhotographyPage.vue
@@ -1,14 +1,49 @@
-<script setup lang="ts">
-import ProjectGrid from '@/components/portfolio/ProjectGrid.vue'
-import { projectsByCategory } from '@/data/projects'
-
-const items = projectsByCategory('photography')
-</script>
-
 <template>
-  <ProjectGrid
-    title="Photography"
-    description="Retratos, calle y series visuales. Seleccion de trabajos recientes."
-    :items="items"
-  />
+  <section class="photography-empty">
+    <p class="photography-hint">click and discover!!</p>
+    <div class="camera-row">
+      <img src="/images/cámara2.png" alt="camara 2" class="camera-image" />
+      <img src="/images/camara4.png" alt="camara 4" class="camera-image" />
+      <img src="/images/cámara1.png" alt="camara 1" class="camera-image" />
+      <img src="/images/camara3.png" alt="camara 3" class="camera-image" />
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.photography-empty {
+  position: relative;
+  min-height: 100vh;
+  background-color: #f8f5ef;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  padding-top: 3rem;
+}
+
+.photography-hint {
+  margin: 0;
+  font-family: Helvetica, Arial, sans-serif;
+  font-style: oblique;
+}
+
+.camera-row {
+  position: absolute;
+  left: 50%;
+  top: 19%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.6rem;
+  padding: 0;
+  box-sizing: border-box;
+  transform: translate(-50%, -50%);
+}
+
+.camera-image {
+  width: 12vw;
+  max-width: 180px;
+  height: auto;
+  object-fit: contain;
+}
+</style>
