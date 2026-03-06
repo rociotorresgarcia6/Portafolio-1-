d515a4eed200544669e2b5da9372fd3af9652085
Rocío Torres <rociotorresgarcia6@gmail.com>
2026-03-06 13:09:26 +0100

    Boton cámara 1

--- src/pages/photography/PhotographyPage.vue
+++ src/pages/photography/PhotographyPage.vue
@@ -1,12 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isFirstCameraOpen = ref(false)
+
+const handleFirstCameraClick = () => {
+  isFirstCameraOpen.value = !isFirstCameraOpen.value
+}
+
+const closeFirstCameraPanel = () => {
+  isFirstCameraOpen.value = false
+}
+</script>
+
 <template>
-  <section class="photography-empty">
+  <section class="photography-empty" @click="closeFirstCameraPanel">
     <p class="photography-hint">click and discover!!</p>
-    <div class="camera-row">
-      <img src="/images/cámara2.png" alt="camara 2" class="camera-image" />
+    <div class="camera-row" @click.stop>
+      <button
+        type="button"
+        class="camera-trigger"
+        :class="{ 'is-open': isFirstCameraOpen }"
+        @click.stop="handleFirstCameraClick"
+      >
+        <img src="/images/cámara2.png" alt="camara 2" class="camera-image" />
+      </button>
       <img src="/images/camara4.png" alt="camara 4" class="camera-image" />
       <img src="/images/cámara1.png" alt="camara 1" class="camera-image" />
       <img src="/images/camara3.png" alt="camara 3" class="camera-image" />
     </div>
+
+    <section v-if="isFirstCameraOpen" class="photo-panel" @click.stop />
   </section>
 </template>
 
@@ -45,5 +68,37 @@
   max-width: 180px;
   height: auto;
   object-fit: contain;
+}
+
+.camera-trigger {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  border: 0;
+  background: transparent;
+  cursor: pointer;
+  transition: transform 180ms ease;
+}
+
+.camera-trigger:hover,
+.camera-trigger.is-open {
+  transform: scale(1.08);
+}
+
+.camera-trigger:focus-visible {
+  outline: 2px solid #111;
+  outline-offset: 4px;
+}
+
+.photo-panel {
+  position: absolute;
+  top: 32%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: min(78vw, 900px);
+  min-height: 470px;
+  border-radius: 14px;
+  background-color: #fff;
 }
 </style>
